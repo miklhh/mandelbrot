31b50f6db59e0ab22aedf9a4b175d6e33d1b4ebd
mikez <mikez@DESKTOP-RTJT31S>
2017-08-24 16:24:49 +0200

    Started adding support for threading.

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,22 @@
 #include <cmath>
 #include <string>
 #include <thread>
+#include <vector>
 
 #include "color.h"
 #include "types.h"
 #include "supersampling.h"
 
-extern const int scr_width = 800;
-extern const int scr_height = 600;
+extern const int scr_width = 600;
+extern const int scr_height = 400;
 uint32_t max_iterations = 40000;
 
 static SDL_Window* window = NULL;
 static SDL_Renderer* renderer = NULL;
 static bool window_is_open = true;
+
+/* Render buffer. */
+color_t render_buffer[scr_height][scr_width];
 
 
 
@@ -26,9 +30,22 @@ void shutdown(std::string str)
 	window_is_open = false;
 }
 
+void draw_mandelbrot_iterate(int y_start, int y_end, scale_t scale, offset_t offset, color_t buffer[scr_height][scr_width])
+{
+    /* Iterate over each pixel on the screen and draw. */
+    for (int px_y = y_start; px_y <= y_end; px_y++)
+    {
+        for (int px_x = 0; px_x < scr_width; px_x++)
+        {
+            /* Get the color for the current pixel and store in the buffer. */
+            color_t color = get_px_4x_ss(px_x, px_y, scale, offset);
+            buffer[px_y][px_x] = color;
+        }
+    }
+}
 
 using std::complex;
-void draw_mandelbrot(complex<double> upper_left, complex<double> lower_right)
+void draw_mandelbrot(complex<double> upper_left, complex<double> lower_right, uint8_t threads)
 { 
     using std::complex;
 
@@ -49,27 +66,57 @@ void draw_mandelbrot(complex<double> upper_left, complex<double> lower_right)
 	scale_x /= scr_width;
 	scale_y /= scr_height;
 	
+
+    /* Create vector of threads. */
+    using std::thread;
+    using std::vector;
+    vector<thread> thread_vector;
+
+    /* Zero the render buffer. */
+    for (int y = 0; y < scr_height; y++)
+    {
+        for (int x = 0; x < scr_width; x++)
+        {
+            render_buffer[y][x] = color_t{ 0, 0, 0 };
+        }
+    }
+    
+    /* Let the threads go to work.*/
+    for (uint8_t i = 0; i < threads; i++)
+    {
+        int lines_per_thread = scr_height / threads;
+        int y_start = i * lines_per_thread;
+        int y_end = (i + 1) * lines_per_thread;
+        thread_vector.push_back( thread(draw_mandelbrot_iterate, y_start, y_end, scale, offset, render_buffer) );
+    }
+
+    while(1)
+    {
+        for (int y = 0; y < scr_height; y++)
+        {
+            for (int x = 0; x < scr_width; x++)
+            {
+                SDL_SetRenderDrawColor(
+                    renderer,
+                    render_buffer[y][x].red,
+                    render_buffer[y][x].green,
+                    render_buffer[y][x].blue,
+                    render_buffer[y][x].alpha);
+                SDL_RenderDrawPoint(renderer, x, y);
+            }
+        }
+        SDL_RenderPresent(renderer);
+        SDL_Delay(20);
+    }
+
+
     /* Iterate over each pixel on the screen and draw. */
 	for (int px_y = 0; px_y < scr_height; px_y++)
 	{
 		for (int px_x = 0; px_x < scr_width; px_x++)
 		{
-            // Debuging.
-            //std::cout << "Drawing pixel, X: " << px_x << ", Y: " << px_y << std::endl;
-			/* Use the scales and offsets to calculate x and y. */
-			double x = double((px_x - scr_width/2) * scale_x + offset_x);
-			double y = double((px_y - scr_height/2) * scale_y + offset_y);
-			complex<double> c(x, y);
-			complex<double> z(0.0);
-
-            color_t color = get_px_4x_ss(px_x, px_y, scale, offset);
-            // Debuging.
-            std::cout
-                << "Color: R: " << (uint32_t)color.red << " "
-                << "Color: G: " << (uint32_t)color.green << " "
-                << "Color: B: " << (uint32_t)color.blue << std::endl;
-
             /* Get the color for the current pixel. */
+            color_t color = get_px_4x_ss(px_x, px_y, scale, offset);
             SDL_SetRenderDrawColor(renderer, color.red, color.green, color.blue, color.alpha);
 			SDL_RenderDrawPoint(renderer, px_x, px_y);
 			SDL_RenderPresent(renderer);
@@ -97,7 +144,7 @@ int main (int argc, char *argv[])
     using std::complex;
     complex<double> ul = std::complex<double>(-0.747, 0.120);
     complex<double> lr = std::complex<double>(-0.746, 0.119);
-    std::thread render(draw_mandelbrot, ul, lr);
+    std::thread render(draw_mandelbrot, ul, lr, 2);
     render.detach();
 
 	while (window_is_open)
